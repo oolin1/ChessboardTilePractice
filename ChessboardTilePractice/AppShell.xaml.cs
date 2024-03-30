a814719526271817b2bcad5f244df6ea6f2f18f9
Marcell Gyurján <gyurjan.marcell@gmail.com>
2024-03-30 19:03:52 +0100

    creating MAUI template

---
+++ ChessboardTilePractice/AppShell.xaml.cs
@@ -0,0 +1,7 @@
+﻿namespace ChessboardTilePractice {
+    public partial class AppShell : Shell {
+        public AppShell() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ChessboardTilePractice/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,8 @@
+﻿using Foundation;
+
+namespace ChessboardTilePractice {
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
