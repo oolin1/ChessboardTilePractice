e507275dd3589da624657465a8318f81b7b88750
Marcell Gyurján <gyurjan.marcell@gmail.com>
2024-03-30 19:07:26 +0100

    cleaning up default view

--- ChessboardTilePractice/MainPage.xaml.cs
+++ ChessboardTilePractice/MainPage.xaml.cs
@@ -1,21 +1,11 @@
 ﻿namespace ChessboardTilePractice {
     public partial class MainPage : ContentPage {
-        int count = 0;
-
         public MainPage() {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e) {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        private void NextSquare_Click(object sender, EventArgs e) {
+            
         }
     }
-
 }
